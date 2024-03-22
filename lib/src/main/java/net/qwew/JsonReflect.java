771d6fff1347c56c8b69d1d6b3a0383da864b17e
Qwewewewew <golubkovivan4@gmail.com>
2024-03-22 13:13:21 +0300

    json to map conversion

--- lib/src/main/java/net/qwew/JsonReflect.java
+++ lib/src/main/java/net/qwew/JsonReflect.java
@@ -1,8 +1,79 @@
 package net.qwew;
 
-public class JsonReflect {
-    
-    public static String test() {
-        return "test";
+import java.lang.reflect.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonReflect<T> {
+
+    private Object object;
+
+    public JsonReflect(Object object) {
+        this.object = object;
     }
+
+    // method to acquire all the fields of an object
+    private Map<String, Object> collectFields() {  
+        Map<String, Object> fields = new HashMap<>();
+        Class<?> classVar = object.getClass();
+        Field[] fieldsArray = classVar.getDeclaredFields();
+        for (Field field : fieldsArray) {
+            field.setAccessible(true);
+            try {
+                fields.put(field.getName(), field.get(object));
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return fields;
+    }     
+    
+    //method to write the json representation of an object
+    public String toJson() {
+        Map<String, Object> fields = collectFields();
+        String jsonRepresentation = "{";
+        for (Map.Entry<String, Object> entry : fields.entrySet()) {
+            jsonRepresentation += "\"" + entry.getKey() + "\":\"" + entry.getValue() + "\",";
+        }
+        jsonRepresentation = jsonRepresentation.substring(0, jsonRepresentation.length() - 1);
+        jsonRepresentation += "}";
+        return jsonRepresentation;
+    }  
+    
+    // save json representation of an object to a file
+    public void saveToFile(String fileName) {
+        try {
+            FileWriter writer = new FileWriter(fileName + ".json");
+            writer.write(toJson());
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Saved to file: " + fileName);
+    }  
+    
+
+    // a method to convert json string to a Map
+    public static Map<String, Object> jsonToMap(String json) {
+        
+        Map<String, Object> map = new HashMap<>();
+
+        String[] keyValuePairs = json.substring(1, json.length() - 1).replace(" ", "").split(",");
+        for (String keyValuePair : keyValuePairs) {
+            String[] keyValue = keyValuePair.split(":");
+            String key = keyValue[0].substring(1, keyValue[0].length() - 1);
+            Object value = keyValue[1].replace("\"", "");
+            map.put(key, value);
+        }
+        
+        return map;
+    }
+
 }
--- lib/src/test/java/net/qwew/JsonReflectTest.java
+++ lib/src/test/java/net/qwew/JsonReflectTest.java
@@ -1,17 +1,33 @@
 package net.qwew;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 public class JsonReflectTest {
-    
-    @Test
-    public void initialTest() {
-        String expected = "test";
 
-        String actual = JsonReflect.test();
-    
-        assertEquals(expected, actual);
+    @Test
+    public void fromJsonTest() {
+        // given
+        String json = "{\"name\": \"John\", \"age\": 30}";
+        JsonReflect<Person> jsonReflect = new JsonReflect<>(new Person("John", 30));
+
+        // when
+        Map<String, Object> fields = jsonReflect.jsonToMap(json);
+
+        // then
+        assertEquals("John", fields.get("name"));
+        assertEquals(30, Integer.parseInt((String)fields.get("age")));
+    }
+
+    private static class Person {
+        private String name;
+        private int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
     }
 }
